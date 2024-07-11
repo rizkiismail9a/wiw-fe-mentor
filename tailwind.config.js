65d2083c68832105b83c7e64180eda0e62678ad5
programmer-fe5 <programmer-fe5@qwertysystem.net>
2024-07-11 09:52:32 +0700

    chore: first config

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        "dark-mode-elements": "hsl(209, 23%, 22%)",
+        "dark-mode-bg": "hsl(207, 26%, 17%)",
+        "light-mode-text": "hsl(200, 15%, 8%)",
+        "light-mode-input": "hsl(0, 0%, 52%)",
+        "light-mode-bg": "hsl(0, 0%, 98%)",
+        "dark-mode-text": "hsl(0, 0%, 100%)",
+        "light-mode-elements": "hsl(0, 0%, 100%)",
+      },
+    },
+  },
+  screens: {
+    sm: "375px",
+  },
+  plugins: [],
+};
